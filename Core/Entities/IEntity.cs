898d3070a1449f044d1aa11d8f1980ecb7cd9f0b
hilalina <hilalina@github.com>
2021-02-26 15:00:37 +0300

    Proje dosyası ekle.

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{                                   //Interface'in içi boşsa IEntity'i implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+
+    }
+}
